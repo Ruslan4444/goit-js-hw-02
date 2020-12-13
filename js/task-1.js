1ff96ca457ec13e7536c18c4bc7fb85452806b99
Ruslan4444 <ruslankv4@gmail.com>
2020-12-13 14:38:54 +0100

    Удосконалено виконання роботи

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
+const logItems = function (array) {
 
-const logItems = function(logItems) {
- for (let i = 0; i < logItems.length; i += 1) {
-    console.log(`${i + 1} - ${logItems[i]}`);
+ for (let i = 0; i < array.length; i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
   }
 };
 
