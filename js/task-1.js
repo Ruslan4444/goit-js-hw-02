687de4d3fc08ba0c3bca3ed671b68ff3c8cb7445
Ruslan4444 <ruslankv4@gmail.com>
2020-12-13 14:26:01 +0100

    Завершено завдання 1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+
+const logItems = function(logItems) {
+ for (let i = 0; i < logItems.length; i += 1) {
+    console.log(`${i + 1} - ${logItems[i]}`);
+  }
+};
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+
+
+
